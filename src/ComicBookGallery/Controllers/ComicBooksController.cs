9e106d167aef2d644696a1bdd3637c09c5ced002
Oisin <oisinmcveigh@hotmail.com>
2016-12-24 21:29:20 +0000

    Added comic book and artist models

--- src/ComicBookGallery/Controllers/ComicBooksController.cs
+++ src/ComicBookGallery/Controllers/ComicBooksController.cs
@@ -15,11 +15,11 @@ namespace ComicBookGallery.Controllers
             ViewBag.Description = "<p>Final issue! Witness the final hours of Doctor Octopus' life and his one, last, great act of revenge! Even if Spider-Man survives... <strong>will Peter Parker?</strong></p>";
             ViewBag.Artists = new string[]
             {
-                "script: dan slott",
-                "pencils: humberto ramos",
-                "inks: victor olazaba",
-                "colors: edgar delgado",
-                "letters: chris eliopoulos"
+                "Script: Dan Slott",
+                "Pencils: Humberto Ramos",
+                "Inks: Victor Olazaba",
+                "Colors: Edgar Delgado",
+                "Letters: Chris Eliopoulos"
             };
             return View();
         }
