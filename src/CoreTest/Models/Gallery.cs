a2aa797a4a35dc022cfb1db6b8fd04f6ef1fbf90
Oisin <oisinmcveigh@hotmail.com>
2017-01-01 20:31:54 +0000

    Added comic book repository

---
+++ src/CoreTest/Models/Gallery.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CoreTest.Models
+{
+    public class Gallery
+    {
+        public string name { get; set; }
+        public Photo[] Photos { get; set; }
+
+    }
+}
---
+++ src/CoreTest/Models/Photo.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CoreTest.Models
+{
+    public class Photo
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+        public string caption { get; set; }
+        public string imageFileName
+        {
+            get
+            {
+                return "banner" + id + ".svg";
+            }
+        }
+    }
+}
